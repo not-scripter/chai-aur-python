7b96245dd7bf9ab8217e3a8b139664ee3170fd30
MRINMOY MONDAL <mm2922894@gmail.com>
2024-06-19 18:39:43 +0530

    test

---
+++ 01_basics/test.py
@@ -0,0 +1,10 @@
+def print_me(name):
+    print(name)
+
+
+print_me("iam test.py")
+
+print_one = "hey iam print one"
+print_two = "hey iam print two"
+print_three = "hey iam print three"
+print_four = "hey iam print four"
---
+++ 01_basics/test_imp.py
@@ -0,0 +1,3 @@
+from test import print_me
+
+print_me("iam test_imp.py")
